89f4e26f79b4c6d8e5268b42c3515047feb4d58f
Kristen <kristen.fyffe@gmail.com>
2019-01-23 21:58:54 +0000

    updated css for nav-bar so that it has z-index of 1

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -49,21 +49,7 @@ const data = [
 $(document).ready(function() {
   console.log('This is the document ready function for the DOM')
 
-  $("i.fas.fa-heart").hide(1000);
-  $("i.fas.fa-flag").hide(1000);
-  $("i.fas.fa-retweet").hide(1000);
 
-  $(".tweet").hover(function () {
-    $(this).addClass("hovertweet");
-    $("i.fas.fa-heart").show(1000);
-    $("i.fas.fa-flag").show(1000);
-    $("i.fas.fa-retweet").show(1000);
-    }, function() {
-      $(this).removeClass("hovertweet");
-      $("i.fas.fa-heart").hide(1000);
-      $("i.fas.fa-flag").hide(1000);
-      $("i.fas.fa-retweet").hide(1000);
-      });
 
 
 // var $tweet = createTweetElement(tweetData);
@@ -75,7 +61,7 @@ $(document).ready(function() {
 function createTweetElement(tweetData) {
 
   //header
-  let avatar = $('<img>').attr("src", tweetData.user.avatars.regular); //variable for the avatar image
+  let avatar = $('<img>').attr("src", tweetData.user.avatars.small); //variable for the avatar image
   let $name = $('<h1>').text(tweetData.user.name);   //variable for the h1 (name) of the person
   let handle = $('<h3>').text(tweetData.user.handle); //variable for the h3 (handle) of the person
   let tweetHeader = $('<header>').append(avatar).append($name).append(handle);//append each variable to the header
@@ -93,14 +79,33 @@ function createTweetElement(tweetData) {
   return $tweet;
 }
 
-function renderTweets(tweet) {
-  tweet.forEach(tweetData => { // forEach...
+function renderTweets(tweets) {
+  console.log('This is tweet: ', tweets);
+  tweets.forEach(tweetData => { // forEach...
+    console.log('This is tweetData: ', tweetData);
     let tweetElement = createTweetElement(tweetData); // calls createTweetElement for each tweet
+    console.log('This is tweetElement: ', tweetElement);
     $('.tweet-container').append(tweetElement); // takes return value and appends it to the tweets container
   });
 }
 
 renderTweets(data);
+
+  $("i.fas.fa-heart").hide(1000);
+  $("i.fas.fa-flag").hide(1000);
+  $("i.fas.fa-retweet").hide(1000);
+
+  $(".tweet").hover(function () {
+    $(this).addClass("hovertweet");
+    $("i.fas.fa-heart").show(1000);
+    $("i.fas.fa-flag").show(1000);
+    $("i.fas.fa-retweet").show(1000);
+    }, function() {
+      $(this).removeClass("hovertweet");
+      $("i.fas.fa-heart").hide(1000);
+      $("i.fas.fa-flag").hide(1000);
+      $("i.fas.fa-retweet").hide(1000);
+      });
 
 });
 
