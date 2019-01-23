91e04e10a01820e67953a061c11a16e7f20dbc10
Kristen <kristen.fyffe@gmail.com>
2019-01-23 02:24:47 +0000

    Updated css for tweet containers, added article with header and footer

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,5 @@
+//When a user types into the Compose Tweet textarea, the Character Counter is updated to show how many characters a user may still type
+// (subtracting the number of characters they've typed from the maximum allowable character count of 140)
 $(document).ready(function() {
   console.log('This is the document ready function for the DOM')
 
@@ -7,9 +9,9 @@ $(document).ready(function() {
     console.log(countdown);
     $(this).siblings('.counter').html(countdown);
     if (countdown < 0) {
-      $(this).siblings('.counter').addClass("overlimit"); //only uses .overlimit class when user has entered too many characters.
+      $(this).siblings('.counter').addClass("overlimit"); //only uses .overlimit class when user has entered too many characters; turns counter red.
     } else {
-      $(this).siblings('.counter').removeClass("overlimit"); //removes .overlimit class when user removes characters and goes back within limit.
+      $(this).siblings('.counter').removeClass("overlimit"); //removes .overlimit class (from red back to gray text) when user removes characters and goes back within limit.
     }
   });
 
