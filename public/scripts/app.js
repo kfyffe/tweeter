546d95a546f7328720adf93985995f5625555b9b
Kristen <kristen.fyffe@gmail.com>
2019-01-23 04:38:36 +0000

    Added icons to tweet list for like, flag, retweet that appear when hover

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,23 +1,21 @@
 $(document).ready(function() {
   console.log('This is the document ready function for the DOM')
 
+  $("i.fas.fa-heart").hide(1000);
+  $("i.fas.fa-flag").hide(1000);
+  $("i.fas.fa-retweet").hide(1000);
+
   $(".tweet").hover(function () {
     $(this).addClass("hovertweet");
+    $("i.fas.fa-heart").show(1000);
+    $("i.fas.fa-flag").show(1000);
+    $("i.fas.fa-retweet").show(1000);
     }, function() {
-    $(this).removeClass("hovertweet");
-    console.log('Is anything changing?');
-  });
-
-    // let value = $( this ).val();
-    // let countdown = 140 - value.length;
-    // console.log(countdown);
-    // $(this).siblings('.counter').html(countdown);
-    // if (countdown < 0) {
-    //   $(this).siblings('.counter').addClass("overlimit"); //only uses .overlimit class when user has entered too many characters; turns counter red.
-    // } else {
-    //   $(this).siblings('.counter').removeClass("overlimit"); //removes .overlimit class (from red back to gray text) when user removes characters and goes back within limit.
-    // }
-
+      $(this).removeClass("hovertweet");
+      $("i.fas.fa-heart").hide(1000);
+      $("i.fas.fa-flag").hide(1000);
+      $("i.fas.fa-retweet").hide(1000);
+      });
 
 });
 
