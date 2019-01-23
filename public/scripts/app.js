4b3b2f5cc683e437f2eb762758a346ddb9e2b5bc
Kristen <kristen.fyffe@gmail.com>
2019-01-23 03:38:31 +0000

    hover functionality added on tweet list articles

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,23 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+$(document).ready(function() {
+  console.log('This is the document ready function for the DOM')
+
+  $(".tweet").hover(function () {
+    $(this).addClass("hovertweet");
+    }, function() {
+    $(this).removeClass("hovertweet");
+    console.log('Is anything changing?');
+  });
+
+    // let value = $( this ).val();
+    // let countdown = 140 - value.length;
+    // console.log(countdown);
+    // $(this).siblings('.counter').html(countdown);
+    // if (countdown < 0) {
+    //   $(this).siblings('.counter').addClass("overlimit"); //only uses .overlimit class when user has entered too many characters; turns counter red.
+    // } else {
+    //   $(this).siblings('.counter').removeClass("overlimit"); //removes .overlimit class (from red back to gray text) when user removes characters and goes back within limit.
+    // }
+
+
+});
 
