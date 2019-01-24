6c321dde49d1846bdac4ed164e8de8d1d7991acd
Kristen <kristen.fyffe@gmail.com>
2019-01-24 21:34:42 +0000

    cleaning up code and beginning work on time since tweet posted functionality

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,40 +1,29 @@
 $(document).ready(function() {
   console.log('This is the document ready function for the DOM')
 
-
-
-
-// var $tweet = createTweetElement(tweetData);
-
-// // // Test / driver code (temporary)
-// console.log($tweet); // to see what it looks like
-// // $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-
+//function that creates a new tweet element with where each piece of data should be appended.
 function createTweetElement(tweetData) {
-
   //header
   let avatar = $('<img>').attr("src", tweetData.user.avatars.small); //variable for the avatar image
   let $name = $('<h1>').text(tweetData.user.name);   //variable for the h1 (name) of the person
   let handle = $('<h3>').text(tweetData.user.handle); //variable for the h3 (handle) of the person
   let tweetHeader = $('<header>').append(avatar).append($name).append(handle);//append each variable to the header
-
   //tweet text (middle)
   let tweetText = $('<p>').text(tweetData.content.text);
-
   //footer
+  let currentTime = Date.now();
+  console.log('Time elapsed is: ', currentTime - tweetData.created_at);
   let postDate = $('<span>').text(tweetData.created_at); //variable for the date the tweet was posted
   let heart = $('<i class="fas fa-heart"></i>');
   let flag = $('<i class="fas fa-flag"></i>');
   let retweet = $('<i class="fas fa-retweet"></i>');
-
   let tweetFooter = $('<footer>').append(postDate).append(heart).append(flag).append(retweet); //append the postDate variable to the footer
-
   //article
   let $tweet = $('<article>').addClass("tweet").append(tweetHeader).append(tweetText).append(tweetFooter);
-
   return $tweet;
 }
 
+//helper function for when existing tweets are hovered. Icons initially hidden, and displayed upon hover...are then removed when mouse is off of the tweet.
 function hoverTweets(tweets) {
   $("i.fas.fa-heart").hide(1000);
   $("i.fas.fa-flag").hide(1000);
@@ -54,39 +43,38 @@ function hoverTweets(tweets) {
       });
 }
 
+//function that renders the tweets and calls createTweetElement function
 function renderTweets(tweets) {
-  console.log('This is tweet: ', tweets);
   $(".tweet").remove();
-  tweets.forEach(tweetData => { // forEach...
-    console.log('This is tweetData: ', tweetData);
-    let tweetElement = createTweetElement(tweetData); // calls createTweetElement for each tweet
-    console.log('This is tweetElement: ', tweetElement);
-    $('.tweet-container').prepend(tweetElement); // takes return value and appends it to the tweets container
+  tweets.forEach(tweetData => {
+    let tweetElement = createTweetElement(tweetData);
+    $('.tweet-container').prepend(tweetElement); // takes return value and prepends it to the tweets container
   });
 }
 
-
+//hides the compose tweet box when first land on the page, after compose button is clicked, tweet box slides into view and focuses the cursor in the text area.
 $(".new-tweet").hide();
 $(".compose").on("click", function (){
 $(".new-tweet").slideToggle();
 $("#tweet-text").focus();
 })
 
+//when cursor is in the textarea, no error message shows
 $('textarea').on("click", function () {
   $(".error").text('');
 })
 
+//functionality for submission of new tweet
 var submitTweet = $('#compose-tweet');
-  submitTweet.on('submit', function (event) {
-    event.preventDefault();
-    let $$tweet = $("#tweet-text").val();
-    console.log ('This is tweet value: ', $$tweet);
-    let newTweetData = $(this).serialize()
-    console.log('newTweetData: ', newTweetData);
-    if ($$tweet === '' || $$tweet === null) {
-      $(".error").text('You did not enter a tweet!');
-    } else if ($$tweet.length > 140) {
-      $(".error").text('Your tweet is too long!');
+submitTweet.on('submit', function (event) {
+  event.preventDefault();
+  let $$tweet = $("#tweet-text").val();
+  let newTweetData = $(this).serialize()
+  //validation for what is entered in the tweet textarea and invokes error message if submitted without tweet or if it is too long.
+  if ($$tweet === '' || $$tweet === null) {
+    $(".error").text('You did not enter a tweet!');
+  } else if ($$tweet.length > 140) {
+    $(".error").text('Your tweet is too long!');
     } else {
       $.post('/tweets', newTweetData)
       .then(newTweetData => {
@@ -94,9 +82,10 @@ var submitTweet = $('#compose-tweet');
         $(".tweet-container").empty();
         loadTweets();
       })
-    }
-  });
+      }
+});
 
+//function with the GET for tweets, calls the renderTweets function and hoverTweets function.
 function loadTweets() {
   $.get('http://localhost:8080/tweets')
   .then(tweets => {
