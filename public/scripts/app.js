b63fe95e323cd8dcd5f5e7248b40b285024e9a6b
Kristen <kristen.fyffe@gmail.com>
2019-01-24 03:03:06 +0000

    hover added to compose button and slideToggle enabled.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -51,16 +51,21 @@ function renderTweets(tweets) {
   $("i.fas.fa-retweet").hide(1000);
 
   $(".tweet").hover(function () {
-    $(this).addClass("hovertweet");
+    $(this).addClass(".hovertweet");
     $("i.fas.fa-heart").show(1000);
     $("i.fas.fa-flag").show(1000);
     $("i.fas.fa-retweet").show(1000);
     }, function() {
-      $(this).removeClass("hovertweet");
+      $(this).removeClass(".hovertweet");
       $("i.fas.fa-heart").hide(1000);
       $("i.fas.fa-flag").hide(1000);
       $("i.fas.fa-retweet").hide(1000);
       });
+
+
+$(".compose").on("click", function (){
+$(".new-tweet").slideToggle();
+})
 
 $(function() {
   var submitTweet = $('#compose-tweet');
